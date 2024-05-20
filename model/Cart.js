30bfe53d13ed224d62d1b4ede5375e4ef0d5ef6c
Chinmay <chinmaybarik214@gmail.com>
2024-05-21 00:30:50 +0530

    8:35:35 before changinf frontend to accomodate populate functionality

---
+++ model/Cart.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const cartSchema = new Schema({
+  quantity: { type: Number, required: true },
+  product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+// DO NOT MESS BELOW
+const virtual = cartSchema.virtual("id");
+virtual.get(function () {
+  return this._id;
+});
+cartSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
+});
+// DO NOT MESS ABOVE
+
+exports.Cart = mongoose.model("Cart", cartSchema);
---
+++ routes/Cart.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { addToCart, fetchCartByUser } = require("../controller/Cart");
+
+const router = express.Router();
+// /products is already added in base path
+router
+  .post("/", addToCart)
+  .get("/", fetchCartByUser)
+
+exports.router = router;
