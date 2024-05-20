e3343fc361c21eadf90b4c7f68d083b1b193bce7
Chinmay <chinmaybarik214@gmail.com>
2024-05-20 18:13:58 +0530

    tut 7:33:48 now using id instead of _id

---
+++ controller/Product.js
@@ -0,0 +1,12 @@
+const { Product } = require("../model/Product");
+
+exports.createProduct = async (req, res) => {
+  // this product we have to get from API body
+  const product = new Product(req.body);
+  try {
+    const doc = await product.save();
+    res.status(201).json(doc)
+  } catch {
+    res.status(400).json(err)
+  }
+};
--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const express = require("express");
 const server = express();
+const mongoose = require("mongoose");
+const { createProduct } = require("./controller/Product");
+
+// middlewares
+server.use(express.json()); // to parse req.body
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+  console.log("database connected");
+}
+
+server.get("/", (req, res) => {
+  res.json({ status: "success" });
+});
+
+server.post("/products", createProduct);
+
+server.listen(8080, () => {
+  console.log("server started");
+});
