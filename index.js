ae62ac0bdaeefdbcd7367c91bff7a0e5198bc9d5
Chinmay <chinmaybarik214@gmail.com>
2024-05-20 20:30:41 +0530

    tut got cors working

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
 const { createProduct } = require("./controller/Product");
-const productsRouter = require('./routes/Products')
-const categoriesRouter = require('./routes/Categories')
-const brandsRouter = require('./routes/Brands')
+const productsRouter = require("./routes/Products");
+const categoriesRouter = require("./routes/Categories");
+const brandsRouter = require("./routes/Brands");
+const cors = require("cors");
 // middlewares
+server.use(
+  cors({
+    exposedHeaders: ["X-Total-Count"],
+  })
+);
 server.use(express.json()); // to parse req.body
-server.use('/products', productsRouter.router);
-server.use('/categories', categoriesRouter.router);
-server.use('/brands', brandsRouter.router);
+server.use("/products", productsRouter.router);
+server.use("/categories", categoriesRouter.router);
+server.use("/brands", brandsRouter.router);
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -20,8 +26,6 @@ async function main() {
 server.get("/", (req, res) => {
   res.json({ status: "success" });
 });
-
-
 
 server.listen(8080, () => {
   console.log("server started");
