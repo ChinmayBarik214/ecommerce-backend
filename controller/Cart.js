4fd027a9c051f927321300c4cd2e114d54344ebd
Chinmay <chinmaybarik214@gmail.com>
2024-05-21 19:15:54 +0530

    tut 8:57:08 qty changing in cart

--- controller/Cart.js
+++ controller/Cart.js
@@ -37,8 +37,10 @@ exports.updateCart = async (req, res) => {
     const cart = await Cart.findByIdAndUpdate(id, req.body, {
       new: true,
     });
-    res.status(200).json(cart);
-  } catch {
+    const result = await cart.populate("product");
+
+    res.status(200).json(result);
+  } catch (err) {
     res.status(400).json(err);
   }
 };
