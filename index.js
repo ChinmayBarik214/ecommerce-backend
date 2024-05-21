2afbe84bda225e1dfab248a588a3693b7ba92508
Chinmay <chinmaybarik214@gmail.com>
2024-05-22 03:03:34 +0530

    tut 9:39:29 before fixing unauthorized bug

--- index.js
+++ index.js
@@ -2,28 +2,87 @@ const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const crypto = require("crypto");
+
 const { createProduct } = require("./controller/Product");
 const productsRouter = require("./routes/Products");
 const categoriesRouter = require("./routes/Categories");
 const brandsRouter = require("./routes/Brands");
-const usersRouter = require("./routes/Users")
-const authRouter = require("./routes/Auth")
-const cartRouter = require("./routes/Cart")
-const ordersRouter = require("./routes/Order")
+const usersRouter = require("./routes/Users");
+const authRouter = require("./routes/Auth");
+const cartRouter = require("./routes/Cart");
+const ordersRouter = require("./routes/Order");
+const { User } = require("./model/User");
 // middlewares
+server.use(
+  session({
+    secret: "keyboard cat",
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+  })
+);
+server.use(passport.authenticate("session"));
+
 server.use(
   cors({
     exposedHeaders: ["X-Total-Count"],
   })
 );
 server.use(express.json()); // to parse req.body
-server.use("/products", productsRouter.router);
+server.use("/products", isAuth, productsRouter.router); // we can also use JWT token
 server.use("/categories", categoriesRouter.router);
 server.use("/brands", brandsRouter.router);
 server.use("/users", usersRouter.router);
 server.use("/auth", authRouter.router);
 server.use("/cart", cartRouter.router);
 server.use("/orders", ordersRouter.router);
+
+// passport strategies
+passport.use(
+  new LocalStrategy(async function (username, password, done) {
+    // by default passport uses username
+    try {
+      const user = await User.findOne({ email: username }).exec();
+      if (!user) {
+        done(null, false, { message: "invalid credentials" }); // for safety
+      }
+      crypto.pbkdf2(
+        password,
+        user.salt,
+        310000,
+        32,
+        "sha256",
+        async function (err, hashedPassword) {
+          if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+            done(null, user); // this line sends to serializer
+          } else {
+            done(null, false, { message: "invalid credentials" });
+          }
+        }
+      );
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+
+// this creates session variable req.user on being called from callbacks
+passport.serializeUser(function (user, cb) {
+  console.log("serialize", user);
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+// this changes session variable req.user when called from authorized request
+passport.deserializeUser(function (user, cb) {
+  console.log("de-serialize", user);
+  process.nextTick(function () {
+    return cb(null, { id: user.id, role: user.role });
+  });
+});
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -34,6 +93,14 @@ async function main() {
 server.get("/", (req, res) => {
   res.json({ status: "success" });
 });
+
+function isAuth(req, res, done) {
+  if (req.user) {
+    done();
+  } else {
+    res.send(401);
+  }
+}
 
 server.listen(8080, () => {
   console.log("server started");
